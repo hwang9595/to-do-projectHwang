2436d5c6bcbc0755877b2b6ca00fc1febd1ecc69
hwang9595 <victorkhvan11@mail.ru>
2026-06-05 16:44:10 +0500

    Update views.py

--- Project1/NewApp/CompList/views.py
+++ Project1/NewApp/CompList/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from HomeList.models import ToDoList  # Импорт из HomeList
+from HomeList.models import ToDoList
 
 def completed_tasks(request):
-    # Только ВЫполненные задачи
     tasks = ToDoList.objects.filter(is_completed=True)
     return render(request, 'complist.html', {'tasks': tasks})
