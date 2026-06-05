d62f93ade7ef6d16eb6d2322606ffc6198a85a32
hwang9595 <victorkhvan11@mail.ru>
2026-06-05 16:43:41 +0500

    Update views.py

--- Project1/NewApp/IncompList/views.py
+++ Project1/NewApp/IncompList/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from HomeList.models import ToDoList  # Импорт из HomeList
+from HomeList.models import ToDoList
 
 def incomplete_tasks(request):
-    # Только НЕвыполненные задачи
+   
     tasks = ToDoList.objects.filter(is_completed=False)
     return render(request, 'incomplist.html', {'tasks': tasks})
